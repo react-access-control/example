a970935d6869aa9ba5441e8345cb1445b242c5a8
ReactAccessControl <accesscontrol@dein-hosting.de>
2020-06-25 15:28:45 +0200

    Intitial project setup

---
+++ config-overrides.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+
+const resolve = (dir) => path.resolve(__dirname, dir);
+
+module.exports = function override(config, env) {
+  config.resolve.plugins = config.resolve.plugins.filter(
+    (plugin) => !(plugin instanceof ModuleScopePlugin)
+  );
+  config.resolve.alias = Object.assign(config.resolve.alias, {
+    '@react-access-control/authentication': resolve('../authentication'),
+    // etc...
+  });
+  return config;
+};
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { ExampleComponent } from '@react-access-control/authentication';
+
+function App() {
+  return (
+    <div className="App">
+      <ExampleComponent text="My Test" />
+    </div>
+  );
+}
+
+export default App;
